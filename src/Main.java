9592242b5eab49e55ad06a439f2603551de21a9c
Andrey Belous <andreyrabo@mail.ru>
2018-12-17 13:50:51 +0300

    cmmt

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
-import syncr.Counter;
-import syncr.CounterThread;
+import training.Callback;
+import training.MyCallable;
+
+import java.util.concurrent.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ThreadLocal<Counter> t = new ThreadLocal<>();
-        t.set(new Counter());
+
+        String result;
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Future future = executor.submit(new MyCallable(new Callback() {
+            @Override
+            public void onSucceed(String str) {
+                System.out.println("CALLBACK");
+                System.out.println(Thread.currentThread().getName());
+                System.out.println(str);
+            }
+        }));
+
+        executor.shutdown();
+
+        System.out.println(Thread.currentThread().getName());
     }
 }
---
+++ src/training/MyCallable.java
@@ -0,0 +1,24 @@
+package training;
+
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<String> {
+
+    public Callback callback;
+
+    public MyCallable(Callback callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    public String call() throws Exception {
+        System.out.println(Thread.currentThread().getName());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        callback.onSucceed("sdfsdf");
+        return "first collable";
+    }
+}
